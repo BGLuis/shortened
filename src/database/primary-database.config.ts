c0707f2444464cd71e943514789c313ca9a49b3f
BGLuis <luishenrique.gbarbosa21@gmail.com>
2025-06-13 15:45:11 -0300

    feat: config aplication

--- src/database/primary-database.config.ts
+++ src/database/primary-database.config.ts
@@ -1,11 +1,16 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { AppConfigService } from 'src/app-config/app-config.service';
+import { DatabaseType } from './database-types';
 
 export const config = (
 	configService: AppConfigService,
 ): TypeOrmModuleOptions => ({
-	type: 'mongodb',
-	database: ':memory:',
+	type: configService.database.type as DatabaseType,
+	host: configService.database.host,
+	port: configService.database.port,
+	// username: configService.database.username,
+	// password: configService.database.password,
+	database: configService.database.name,
 	entities: [__dirname + '/../**/*.entity{.ts,.js}'],
 	synchronize: true,
 });
