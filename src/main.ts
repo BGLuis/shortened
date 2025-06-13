196d234881d80891cb5ad29382119283570b17f6
BGLuis <luishenrique.gbarbosa21@gmail.com>
2025-06-13 15:44:37 -0300

    feat: config pipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+	configPipe(app);
 	await app.listen(3000);
 }
+
+function configPipe(app: INestApplication) {
+	app.useGlobalPipes(
+		new ValidationPipe({
+			transform: true,
+			whitelist: true,
+		}),
+	);
+}
+
 bootstrap();
